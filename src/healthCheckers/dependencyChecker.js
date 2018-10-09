624e5e6f16d900ad918df4f58a238580d4733f3c
gal.cohen <gal.cohen@zooz.com>
2018-10-09 17:21:14 +0300

    change connect event and change interval to settimeout

--- src/healthCheckers/dependencyChecker.js
+++ src/healthCheckers/dependencyChecker.js
@@ -10,29 +10,36 @@ module.exports = class DependencyChecker {
             resumePauseCheckFunction: config.ResumePauseCheckFunction,
             resumePauseIntervalMs: config.ResumePauseIntervalMs,
             consumer: consumer,
-            state: true
+            state: true,
+            keepHandleResumePause: true
         });
 
-        this.intervalId = setInterval(function(){
-            this.resumePauseCheckFunction(this.consumer)
-                .then((shouldResume) => {
-                    if (shouldResume) {
-                        this.logger.trace('ran ResumePauseCheckFunction and got should resume. will try to resume consumer if it was stopped');
-                        this.consumer.setDependencyHealthy(true);
-                        this.consumer.resume();
-                    } else {
-                        this.logger.trace('ran ResumePauseCheckFunction and got should pause, will pause consumer if it was running');
-                        this.consumer.setDependencyHealthy(false);
-                        this.consumer.pause();
-                    }
-                })
-                .catch(err => {
-                    this.logger.error(err, 'ResumePauseCheckFunction was rejected');
-                });
-        }.bind(this), this.resumePauseIntervalMs);
+        setTimeout(handleResumePauseFunc, this.resumePauseIntervalMs, this);
     }
 
     stop() {
-        clearInterval(this.intervalId);
+        this.keepHandleResumePause = false;
+    }
+};
+
+let handleResumePauseFunc = function(depChecker){
+    if (depChecker.keepHandleResumePause) {
+        depChecker.resumePauseCheckFunction(depChecker.consumer)
+            .then((shouldResume) => {
+                if (shouldResume) {
+                    depChecker.logger.trace('ran ResumePauseCheckFunction and got should resume. will try to resume consumer if it was stopped');
+                    depChecker.consumer.setDependencyHealthy(true);
+                    depChecker.consumer.resume();
+                } else {
+                    depChecker.logger.trace('ran ResumePauseCheckFunction and got should pause, will pause consumer if it was running');
+                    depChecker.consumer.setDependencyHealthy(false);
+                    depChecker.consumer.pause();
+                }
+            })
+            .catch(err => {
+                depChecker.logger.error(err, 'ResumePauseCheckFunction was rejected');
+            }).then(() => { // finally
+                setTimeout(handleResumePauseFunc, depChecker.resumePauseIntervalMs, depChecker);
+            });
     }
 };
